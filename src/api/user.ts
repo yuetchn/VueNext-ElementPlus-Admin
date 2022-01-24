84b5fba46a8f542937a946320db11c6565d6f6e1
YuetChn <yuetchn@163.com>
2022-01-24 17:31:24 +0800

    更新

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,10 +1,11 @@
 /*
  * @ModuleName: User
  * @Author: 乐涛
- * @LastEditTime: 2022-01-23 13:26:11
+ * @LastEditTime: 2022-01-24 17:29:24
  */
 import req from "@/utils/request";
 
+/** 获取用户数据 */
 export const GetUserData = (data:any) => req.Post("user/getUserData", data);
-
+/** 获取当前登录用户信息 */
 export const GetUserInfo = () => req.Get("user/getUserInfo")

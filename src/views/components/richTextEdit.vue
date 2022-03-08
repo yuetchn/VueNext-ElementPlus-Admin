6061412343a316e5c836b2d30665212048f6eb15
YuetChn <yuetchn@163.com>
2022-03-08 18:00:27 +0800

    更新

--- src/views/components/richTextEdit.vue
+++ src/views/components/richTextEdit.vue
@@ -1,14 +1,16 @@
 <!--
  * @ModuleName: richTextEdit
  * @Author: yuetchn@163.com
- * @LastEditTime: 2022-02-28 15:24:37
+ * @LastEditTime: 2022-03-08 17:28:18
 -->
 <template>
   <div>
     <el-card>
       <template #header> 使用富文本 </template>
-      <div>
-        <g-editor v-model="text"></g-editor>
+      <div style="height:300px">
+        <el-input v-model="text" placeholder=""></el-input>
+        <button @click="test">插入内容</button>
+        <g-editor ref="editor" v-model="text"></g-editor>
       </div>
     </el-card>
     <el-card style="margin-top: 15px">
@@ -23,29 +25,39 @@ import help from "./index";
 
 const data = ref([
   {
+    name: "id",
+    desc: "Dom ID",
+    type: "string",
+    default: "editor",
+  },
+  {
     name: "v-model",
     desc: "内容绑定",
     type: "any",
     default: "",
   },
   {
-    name: "tag-name",
-    desc: "生成指定tag标签",
-    type: "string",
-    default: "div",
-  },
-  {
     name: "width",
     desc: "编辑器宽度",
-    type: "number",
-    default: "0",
+    type: "string | number",
+    default: "",
   },
   {
     name: "height",
     desc: "编辑器高度",
-    type: "number",
+    type: "string | number",
+    default: "",
+  },
+  {
+    name: "min-height",
+    desc: "编辑器最小高度",
+    type: "string | number",
     default: "300",
   },
 ]);
 const text = ref("<p>测试文档编辑</p>");
+const editor = ref()
+const test = () => {
+  editor.value.insertContent("测试")
+}
 </script>

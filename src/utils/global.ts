626c417e5fe91598a317804cde7e6bd765e21c1d
YuetChn <yuetchn@163.com>
2022-03-08 09:01:03 +0800

    新增插件：xss 防止注入

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,13 +1,17 @@
 /*
  * @ModuleName: 全局属性挂载
  * @Author: yuetchn@163.com
- * @LastEditTime: 2022-02-28 11:15:18
+ * @LastEditTime: 2022-03-08 08:59:46
  */
 import { App } from "vue";
+import xss from "xss";
 import axios from "@/utils/request";
 import * as func from "@/utils/func";
 
+// 挂载后需要挂载声明
+// src > types > global > globalInterface.d.ts 路径
 export default (app: App) => {
   app.config.globalProperties.$http = axios;
   app.config.globalProperties.$func = func;
+  app.config.globalProperties.$xss = xss;
 };

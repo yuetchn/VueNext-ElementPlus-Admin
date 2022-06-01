a9ce78076bd762d0fa2676d198c54317419c4451
乐涛 <yuetchn@163.com>
2022-06-01 14:49:54 +0800

    import 调整

---
+++ src/utils/func/src/clipboard.ts
@@ -0,0 +1,26 @@
+/*
+ * @ModuleName: Clipboard
+ * @Author: yuetchn@163.com
+ * @LastEditTime: 2022-06-01 14:45:12
+ */
+
+/** 写入剪切板 */
+export const ClipboardFunc = (data: string) => {
+  if (navigator.clipboard) {
+    return navigator.clipboard.writeText(data);
+  }
+  
+  const textDom = document.createElement("textarea");
+  textDom.innerText = data;
+  textDom.style.zIndex = "0";
+  textDom.style.top = "-999999px";
+  textDom.style.left = "-999999px";
+  textDom.style.position = "absolute";
+  document.body.appendChild(textDom);
+  textDom.select();
+  const copyResult = document.execCommand("copy");
+  setTimeout(() => {
+    document.body.removeChild(textDom);
+  }, 100);
+  return copyResult ? Promise.resolve() : Promise.reject();
+};
